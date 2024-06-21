dbbc81d91c1d2c7c27272e267f9d056831fe259e
Matthias Hölzl <tc@xantira.com>
2024-06-21 11:18:27 +0200

    Improve Word table output some more

--- src/claas/table_word_converter.py
+++ src/claas/table_word_converter.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET  # noqa
+from pathlib import Path
 
 from docx import Document
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
@@ -13,6 +14,7 @@ class TableWordConverter(CurriculumConverter):
     def __init__(self, tree: ET.ElementTree):
         super().__init__(tree)
         self._current_table = None
+        self._current_table_total_duration = 0
         self._need_new_table = True
         self._last_output = None
 
@@ -26,16 +28,20 @@ class TableWordConverter(CurriculumConverter):
         if self._need_new_table:
             self._create_new_table(output)
             self._need_new_table = False
+        self._current_table_total_duration += int(duration)
         self._current_table.add_row()
         row_cells = self._current_table.rows[-1].cells
         row_cells[0].text = contents
         row_cells[1].text = str(duration)
+        row_cells[1].paragraphs[0].alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
         row_cells[2].text = methodik
         row_cells[3].text = material
+        self._set_cell_margins(row_cells)
         self._last_output = "row"
 
     def add_remark(self, output, bemerkung):
         if self._last_output == "row":
+            self._add_table_footer()
             output.add_paragraph()  # Add space between tables
         # Add the remark as a bold paragraph
         p = output.add_paragraph()
@@ -54,7 +60,22 @@ class TableWordConverter(CurriculumConverter):
         hdr_cells[2].text = "Methodik/Didaktik"
         hdr_cells[3].text = "Materialien"
         self._set_header_style(hdr_cells)
+        self._set_cell_margins(hdr_cells)
         self._need_new_table = False
+        self._current_table_total_duration = 0
+        self._last_output = "table"
+
+    def _add_table_footer(self):
+        # Write a table footer with the total duration
+        self._current_table.add_row()
+        row_cells = self._current_table.rows[-1].cells
+        row_cells[0].text = "Summe:"
+        row_cells[1].text = str(self._current_table_total_duration)
+        for i in range(2, 4):
+            row_cells[i].text = ""
+        self._set_footer_style(row_cells)
+        row_cells[0].paragraphs[0].alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
+        self._set_cell_margins(row_cells)
 
     @staticmethod
     def _set_header_style(cells):
@@ -67,6 +88,40 @@ class TableWordConverter(CurriculumConverter):
             cell_shading = OxmlElement("w:shd")
             cell_shading.set(qn("w:fill"), "D9D9D9")  # Light grey background
             cell._element.get_or_add_tcPr().append(cell_shading)  # noqa
+
+    @staticmethod
+    def _set_footer_style(cells):
+        for cell in cells:
+            cell_paragraph = cell.paragraphs[0]
+            cell_paragraph.alignment = (
+                WD_PARAGRAPH_ALIGNMENT.RIGHT
+                if cell.text == "Summe:"
+                else WD_PARAGRAPH_ALIGNMENT.CENTER
+            )
+            run = cell_paragraph.runs[0]
+            run.bold = False
+            run.font.size = Pt(11)
+            cell_shading = OxmlElement("w:shd")
+            cell_shading.set(qn("w:fill"), "D9D9D9")  # Light grey background
+            cell._element.get_or_add_tcPr().append(cell_shading)  # noqa
+
+    @staticmethod
+    def _set_cell_margins(cells, top=100, start=100, bottom=100, end=100):
+        for cell in cells:
+            tc = cell._element
+            tcPr = tc.get_or_add_tcPr()
+            tcMar = OxmlElement("w:tcMar")
+            for margin, value in [
+                ("top", top),
+                ("start", start),
+                ("bottom", bottom),
+                ("end", end),
+            ]:
+                node = OxmlElement(f"w:{margin}")
+                node.set(qn("w:w"), str(value))
+                node.set(qn("w:type"), "dxa")
+                tcMar.append(node)
+            tcPr.append(tcMar)
 
     def finalize_output(self, output):
         return output
