5727cba226ed8ce7b82d3d2c4c40ae6dd39ff3df
Matthias Hölzl <tc@xantira.com>
2024-06-21 10:52:00 +0200

    Improve Word table output

--- src/claas/table_word_converter.py
+++ src/claas/table_word_converter.py
@@ -12,7 +12,9 @@ from claas.curriculum_converter import CurriculumConverter
 class TableWordConverter(CurriculumConverter):
     def __init__(self, tree: ET.ElementTree):
         super().__init__(tree)
-        self.current_table = None
+        self._current_table = None
+        self._need_new_table = True
+        self._last_output = None
 
     def start_output(self):
         return Document()
@@ -21,33 +23,38 @@ class TableWordConverter(CurriculumConverter):
         self._create_new_table(output)
 
     def add_topic(self, output, contents, duration, methodik, material):
-        self.current_table.add_row()
-        row_cells = self.current_table.rows[-1].cells
+        if self._need_new_table:
+            self._create_new_table(output)
+            self._need_new_table = False
+        self._current_table.add_row()
+        row_cells = self._current_table.rows[-1].cells
         row_cells[0].text = contents
         row_cells[1].text = str(duration)
         row_cells[2].text = methodik
         row_cells[3].text = material
+        self._last_output = "row"
 
     def add_remark(self, output, bemerkung):
-        if hasattr(self, "current_table"):
+        if self._last_output == "row":
             output.add_paragraph()  # Add space between tables
         # Add the remark as a bold paragraph
         p = output.add_paragraph()
         run = p.add_run(bemerkung)
         run.bold = True
         p.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
-
-        self._create_new_table(output)
+        self._need_new_table = True
+        self._last_output = "remark"
 
     def _create_new_table(self, output):
-        self.current_table = output.add_table(rows=1, cols=4)
-        self.current_table.style = "Table Grid"
-        hdr_cells = self.current_table.rows[0].cells
+        self._current_table = output.add_table(rows=1, cols=4)
+        self._current_table.style = "Table Grid"
+        hdr_cells = self._current_table.rows[0].cells
         hdr_cells[0].text = "Inhalt"
         hdr_cells[1].text = "UE"
         hdr_cells[2].text = "Methodik/Didaktik"
         hdr_cells[3].text = "Materialien"
         self._set_header_style(hdr_cells)
+        self._need_new_table = False
 
     @staticmethod
     def _set_header_style(cells):
